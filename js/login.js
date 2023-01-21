faaeb8cfe167cb7d1c9a08981767e61808ba4422
walaa obeidat <wal.obeidat@gmail.com>
2023-01-21 23:47:57 +0300

    third step

---
+++ js/login.js
@@ -0,0 +1,28 @@
+let formLogInEvent = document.getElementById("sub")
+let users = JSON.parse(localStorage.getItem("users")) || [];
+console.log(users)
+let logform = document.getElementById('logform')
+let mail = document.getElementById('mail')
+let pass = document.getElementById('pass')
+logform.addEventListener('submit', (e)=>{
+    let emailValue = mail.value
+    let passValue = pass.value
+    users.map(e => {
+        console.log(e.email == emailValue, e.password == passValue)
+        if(e.email == emailValue && e.password == passValue){
+            localStorage.setItem('current', JSON.stringify(e.email))
+            console.log('welcome')
+
+            const url = getCurrentURL()
+            loc = url.match(/.*?(?=\/login\/login\.html|$)/i)[0]
+            location.href = `${loc}/main/mainhome.html`
+        }else{
+            console.log('check your password')
+        }
+    })
+    e.preventDefault()
+})
+
+function getCurrentURL () {
+    return window.location.href
+}
---
+++ js/sigin.js
@@ -0,0 +1,68 @@
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+let validError = []
+function logSubmit() {
+        var fname = document.getElementById('fname');
+        var num = document.getElementById('num');
+        var email = document.getElementById('email');
+        var password = document0.getElementById('password');
+        var conpass = document.getElementById('conpass');
+        var form = document.getElementById('form');
+        var namecheck = /\d/;
+        var y = /\w+/;
+        var numcheck = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+        var emailcheck = /^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/;
+        if (namecheck.test(fname.value) || namecheck == ''){
+           document.getElementById('fnameerror').innerHTML ="name should not contain numbers";
+           document.getElementById ('frameerror').style.fontsize = '16px'
+           document.getElementById('frameerror').style.color ='red'
+           validError.push('name')
+        } else if(y.test(fname.value)){
+          document.getElementById('fnameerror').innerHTML= "";
+        }
+        if(numcheck.test(num.value)){
+          document.getElementById('numerror').innerHTML = "";
+        } else {
+            document.getElementById('numerror').innerHTML = "**Should not contain digits and special characters";
+            validError.push('number')
+        }
+        if (emailcheck.test(email.value)) {
+            document.getElementById('mail').innerHTML = "";
+        } else {
+            document.getElementById('mail').innerHTML = "**Email-id is invalid";
+            validError.push('email')
+        }
+        if(validError.length == 0){
+            console.log(fname.value , num.value, email.value, password.value, conPass.value)
+            let obj = {
+                        name: fname.value,
+                        email: email.value,
+                        num: num.value,
+                        password: password.value,
+                        confirmtionPass: conPass.value
+                    }
+                    users.push(obj)
+                    localStorage.setItem("users", JSON.stringify(users));
+                    fname.value = ''
+                    email.value = ''
+                    num.value = ''
+                    password.value = ''
+                    conPass.value = ''
+        }
+}
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
